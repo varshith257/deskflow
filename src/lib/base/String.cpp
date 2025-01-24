03f3270bce05213ac0e7fcaee04dda924e6bc9ef
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2025-01-24 20:10:58 +0530

    add few files

--- src/lib/base/String.cpp
+++ src/lib/base/String.cpp
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#define ARCH (Arch::getInstance())
 
 #include "base/String.h"
 #include "arch/Arch.h"
